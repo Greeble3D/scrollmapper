2284c1ee48301d73771dca78002dcd94ed472764
LB3D <85522485+LeoBlanchette@users.noreply.github.com>
2025-01-05 10:26:47 -0600

    Updates to reader.

--- scenes/modules/reader/reader.gd
+++ scenes/modules/reader/reader.gd
@@ -8,31 +8,44 @@ const READER_CHAPTER_BUTTON = preload("res://scenes/modules/reader/reader_chapte
 
 @export var books_button: Button 
 @export var previous_chapter_button: Button 
+@export var cross_refs_button: Button 
 @export var next_chapter_button: Button 
+@export var exit_button: Button 
 
 @export var chapters_v_box_container: VBoxContainer 
 @export var reading_v_box_container: VBoxContainer 
 @export var cross_referencing_margin_container: MarginContainer
 @export var cross_referencing_v_box_container: VBoxContainer
-
 @export var reader_book_selector: ReaderBooksSelector
+
+@export var chapers_scroll_container: ScrollContainer 
+@export var reading_scroll_container: ScrollContainer 
+@export var cross_referencing_scroll_container: ScrollContainer 
+
 
 var current_translation:String = "KJV"
 var current_book:String = "Genesis"
 var current_chapter:int = 1
 
-var mininum_chapter:int = 1
+var minimum_chapter:int = 1
 var maximum_chapter:int = 1
 
 func _ready() -> void:
+	ScriptureService.verse_cross_references_searched.connect(_on_cross_references_received)
 	books_button.pressed.connect(show_reader_book_selector)
 	reader_book_selector.book_chosen.connect(_on_book_chosen)
+	cross_refs_button.pressed.connect(toggle_cross_references)
 	previous_chapter_button.pressed.connect(previous_chapter)
 	next_chapter_button.pressed.connect(next_chapter)
+	exit_button.pressed.connect(back_to_home)
+	
 	hide_reader_book_selector()
 	show_chapter("KJV", "Genesis", 1)
 	hide_cross_referencing_browser()
 
+## Show the chapter for the given translation, book, and chapter
+## The main chapter display function. Use it for the reader from any 
+## other chapter-display need.
 func show_chapter(translation_abbr:String, book:String, chapter:int = 1) -> void:
 	current_translation = translation_abbr
 	current_book = book
@@ -46,18 +59,21 @@ func show_chapter(translation_abbr:String, book:String, chapter:int = 1) -> void
 	for verse:Dictionary in verses:
 		var reading_scripture:ReadingScripture = create_reading_scripture(verse)
 		reading_v_box_container.add_child(reading_scripture)
-	
+	reading_scroll_container.scroll_vertical = 0
 
+## Show the next chapter
 func next_chapter() -> void:
 	if current_chapter < maximum_chapter:
 		current_chapter += 1
 		show_chapter(current_translation, current_book, current_chapter)
 
+## Show the previous chapter
 func previous_chapter() -> void:
-	if current_chapter > mininum_chapter:
+	if current_chapter > minimum_chapter:
 		current_chapter -= 1
 		show_chapter(current_translation, current_book, current_chapter)
 
+## Populate the chapter buttons
 func populate_chapter_buttons():
 	clear_chapters_browser()
 	var chapters:Dictionary = ScriptureService.get_all_chapter_numbers_in_book(current_translation, current_book )
@@ -65,40 +81,73 @@ func populate_chapter_buttons():
 		var reader_chapter_button:ReaderChapterButton = create_reader_chapter_button(current_translation, current_book, chapter)
 		chapters_v_box_container.add_child(reader_chapter_button)
 	update_min_max_chapters_for_book(chapters["chapters"])
+	chapers_scroll_container.scroll_vertical = 0
 
+## Update the minimum and maximum chapters for the current book
 func update_min_max_chapters_for_book(chapters:Array) -> void:
-	mininum_chapter = 10000
+	minimum_chapter = 10000
 	maximum_chapter = 1
 	for chapter in chapters:
-		if chapter < mininum_chapter:
-			mininum_chapter = chapter
+		if chapter < minimum_chapter:
+			minimum_chapter = chapter
 		if chapter > maximum_chapter:
 			maximum_chapter = chapter
 
+## Request cross-references for the given translation, book, chapter, and verse
+func request_cross_references(_translation:String, _book_name:String, _chapter:int, _verse:int) -> void:
+	ScriptureService.request_cross_references(_translation, _book_name, _chapter, _verse)
+
+func _on_cross_references_received(cross_references:Array) -> void:
+	clear_cross_referencing_browser()
+	for cross_reference in cross_references:
+		var reading_scripture:ReadingScripture = create_reading_scripture(cross_reference)
+		reading_scripture.convert_to_cross_reference()
+		cross_referencing_v_box_container.add_child(reading_scripture)
+	show_cross_referencing_browser()
+	cross_referencing_scroll_container.scroll_vertical = 0
+
+## Toggle the cross-referencing container
+func toggle_cross_references() -> void:
+	if cross_referencing_margin_container.visible:
+		hide_cross_referencing_browser()
+	else:
+		show_cross_referencing_browser()
+
+## Show the reader
 func show_reader_book_selector() -> void:
 	reader_book_selector.show()
 
+## Hide the reader
 func hide_reader_book_selector() -> void:
 	reader_book_selector.hide()
 
+## Clear the chapters browser
 func clear_chapters_browser() -> void:
 	for child in chapters_v_box_container.get_children():
 		child.queue_free()
 
+## Clear the reading browser
 func clear_reading_browser() -> void:
 	for child in reading_v_box_container.get_children():
 		child.queue_free()
 
+## Clear the cross-referencing browser
 func clear_cross_referencing_browser() -> void:
 	for child in cross_referencing_v_box_container.get_children():
 		child.queue_free()
 
+## Hide the cross-referencing browser
 func hide_cross_referencing_browser():
 	cross_referencing_margin_container.hide()
 
+## Show the cross-referencing browser
 func show_cross_referencing_browser():
 	cross_referencing_margin_container.show()
 
+## Create a reading scripture object
+## This is the scripture for reading.
+## It is for reading a scripture.
+## More documentation here: https://www.youtube.com/watch?v=CLeJ-tZBqKY 
 func create_reading_scripture(verse:Dictionary) -> ReadingScripture:
 	var rs:ReadingScripture = READING_SCRIPTURE.instantiate()
 	rs.translation_id = verse["translation_id"]
@@ -108,10 +157,11 @@ func create_reading_scripture(verse:Dictionary) -> ReadingScripture:
 	rs.chapter = verse["chapter"]
 	rs.verse = verse["verse"]
 	rs.verse_id = verse["verse_id"]
-	rs.verse_hash = verse["verse_hash"]
+	#rs.verse_hash = verse["verse_hash"]
 	rs.text = verse["text"]
 	rs.title = verse["title"]
 	rs.license = verse["license"]
+	rs.scripture_pressed.connect(request_cross_references)
 	return rs
 	
 func create_reading_title(title:String) -> ReadingTitle:
@@ -129,3 +179,6 @@ func create_reader_chapter_button(translation:String, book:String, chapter:int) 
 	
 func _on_book_chosen(translation_abbr:String, book:String) -> void:
 	show_chapter(translation_abbr, book, 1)
+
+func back_to_home() -> void:
+	GameManager.load_level("HOME")
--- scenes/modules/reader/reading_scripture/reading_scripture.gd
+++ scenes/modules/reader/reading_scripture/reading_scripture.gd
@@ -20,11 +20,33 @@ var translation_abbr: String = ""
 var title: String = ""
 var license: String = ""
 
+@export var interaction_button: Button
 @export var verse_number_text_rich_text_label: RichTextLabel 
 @export var text_rich_text_label: RichTextLabel
+
+var is_cross_reference_scripture:bool = false:
+	set(value):
+		is_cross_reference_scripture = value
+		if value:
+			convert_to_cross_reference()
+
+signal scripture_pressed(translation_abbr:String, book_name:String, chapter:int, verse:int)
+
+func _ready() -> void:
+	interaction_button.pressed.connect(_on_scripture_pressed)
 
 func set_verse_number(verse_number:int) -> void:
 	verse_number_text_rich_text_label.text = str(verse_number)
 
 func set_verse_text(text:String) -> void:
 	text_rich_text_label.text = text
+
+func convert_to_cross_reference() -> void:
+	verse_number_text_rich_text_label.hide()
+	var scripture_location:String = " - %s %s:%s [%s]"%[
+		book_name, str(chapter), str(verse), translation_abbr
+	]
+	text_rich_text_label.text = text_rich_text_label.text + scripture_location
+
+func _on_scripture_pressed() -> void:
+	scripture_pressed.emit(translation_abbr, book_name, chapter, verse)
